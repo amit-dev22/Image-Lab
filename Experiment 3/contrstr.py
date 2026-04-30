20256221d443fed91847a44ec8bd82955fafa409
amit-dev22 <amitrao8368@gmail.com>
2026-04-30 11:10:00 +0530

    Update image path in contrstr.py

--- Experiment 3/contrstr.py
+++ Experiment 3/contrstr.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-img = cv2.imread(r'C:/Users/kirti/OneDrive/Desktop/image processing lab/im.jpeg', 0)
+img = cv2.imread(r'C:/Users/amit/OneDrive/Desktop/image processing lab/im.jpeg', 0)
 
 min_val, max_val = np.min(img), np.max(img)
 
